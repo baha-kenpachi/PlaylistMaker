55d853f028829548936246e8a4981daa63425105
baha-kenpachi <kenpachi03@yandex.ru>
2023-06-09 22:22:11 +0500

    deleted @SuppressLint("MissingInflatedId")

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -7,7 +7,6 @@ import android.os.Bundle
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
