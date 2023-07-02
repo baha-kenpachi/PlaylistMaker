b73b3dd383911b1927245eb5571801f662906183
baha-kenpachi <kenpachi03@yandex.ru>
2023-07-02 19:17:13 +0500

    added track list

---
+++ app/src/main/java/com/example/playlistmaker/searchertrack/TrackAdapter.kt
@@ -0,0 +1,23 @@
+package com.example.playlistmaker.searchertrack
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.playlistmaker.R
+
+class TrackAdapter(
+    private val data: List<TrackData>
+) : RecyclerView.Adapter<TrackViewHolder> () {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.track_view, parent, false)
+        return TrackViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: TrackViewHolder, position: Int) {
+        holder.bind(data[position])
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+}
