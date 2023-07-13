5c5b838e94a65d33783d00790b0c21ee174d1cb8
baha-kenpachi <kenpachi03@yandex.ru>
2023-07-13 19:52:03 +0500

    Init commit sprint 11 started

--- app/src/main/java/com/example/playlistmaker/searchertrack/TrackViewHolder.kt
+++ app/src/main/java/com/example/playlistmaker/searchertrack/TrackViewHolder.kt
@@ -1,6 +1,8 @@
 package com.example.playlistmaker.searchertrack
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
@@ -8,7 +10,11 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.example.playlistmaker.R
 
-class TrackViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+//class TrackViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
+class TrackViewHolder(parentView: ViewGroup) : RecyclerView.ViewHolder(
+    LayoutInflater.from(parentView.context)
+        .inflate(R.layout.track_view, parentView, false)
+) {
     //private val rootTrackLayout: ConstraintLayout = itemView.findViewById(R.id.rootTrackLayout)
     private val ivTrack : ImageView = itemView.findViewById(R.id.ivTrack)
     private val tvTrackName : TextView = itemView.findViewById(R.id.tvTrackName)
