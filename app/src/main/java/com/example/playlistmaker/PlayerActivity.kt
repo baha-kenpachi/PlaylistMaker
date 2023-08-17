43295b71829c1d72fae257b456dd1c5c623ff2c8
baha-kenpachi <kenpachi03@yandex.ru>
2023-08-18 01:02:44 +0500

    refactored some code

--- app/src/main/java/com/example/playlistmaker/PlayerActivity.kt
+++ app/src/main/java/com/example/playlistmaker/PlayerActivity.kt
@@ -1,15 +1,18 @@
 package com.example.playlistmaker
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.util.DisplayMetrics
 import android.util.Log
 import android.view.View
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.example.playlistmaker.searchertrack.TrackData
+
 
 class PlayerActivity : AppCompatActivity() {
 
@@ -56,15 +59,16 @@ class PlayerActivity : AppCompatActivity() {
             Log.d("TrackOnPlayer", "$track")
         }
     }
-
-
+    fun convertPixelsToDp(px: Float, context: Context): Float {
+        return px / (context.resources.displayMetrics.densityDpi.toFloat() / DisplayMetrics.DENSITY_DEFAULT)
+    }
     private fun bind(track: TrackData) {
 
             Glide.with(this)
                 .load(track.artworkUrl512)
                 .placeholder(R.drawable.placeholder_not_found)
                 .fitCenter()
-                .transform(RoundedCorners(16))
+                .transform(RoundedCorners(convertPixelsToDp(32.0f, this).toInt()))
                 .into(albumCover)
 
             timeCounter.text = track.trackTime
